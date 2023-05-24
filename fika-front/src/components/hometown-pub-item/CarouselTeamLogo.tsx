d25554ce397c60d14ca53adcd669595bea5602a5
DevSaurosYoony <by.cho.dev@gmail.com>
2023-05-24 13:30:03 +0900

    add Carousel func

--- fika-front/src/components/hometown-pub-item/CarouselTeamLogo.tsx
+++ fika-front/src/components/hometown-pub-item/CarouselTeamLogo.tsx
@@ -4,17 +4,17 @@ import clsx from "clsx";
 const CarouselTeamLogo = () => {
 
   const moveLeft = () => {
-    document.getElementById("content").scrollLeft -= 100;
+    document.getElementById("content").scrollLeft -= 400;
   }
 
   const moveRight = () => {
-    document.getElementById("content").scrollLeft += 100;
+    document.getElementById("content").scrollLeft += 400;
   }
 
   return (
   <div>
-    <button onClick={moveLeft} className="text-black">1</button>
-    <button onClick={moveRight} className="text-black">2</button>
+    <button onClick={moveLeft} className="text-black">{"<"}</button>
+    <button onClick={moveRight} className="text-black">{">"}</button>
     <div id="content" className="carousel w-full p-3 flex overflow-x-auto gap-2"> 
       <div className="w-[86px] h-[79px] min-w-[86px] justify-center items-center border-gray-bg border-2 text-black">전체0</div>
       <div className="w-[86px] h-[79px] min-w-[86px] justify-center items-center border-gray-bg border-2 text-black">전체1</div>
