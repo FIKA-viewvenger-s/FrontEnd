e98c27857d1084bd517d9d0a1debbf33bd656631
wonhee418 <cheeky4@naver.com>
2023-06-11 01:56:36 +0900

    design : 캐러셀 css 수정

--- fika-front/src/components/hometown-pub-item/CarouselTeamLogo.tsx
+++ fika-front/src/components/hometown-pub-item/CarouselTeamLogo.tsx
@@ -4,22 +4,25 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import Carousel from "react-elastic-carousel";
 
-
 const breakPoints = [
   { width: 1, itemsToShow: 5 },
   { width: 550, itemsToShow: 2, itemsToScroll: 2 },
   { width: 768, itemsToShow: 3 },
-  { width: 1200, itemsToShow: 4 }
+  { width: 1200, itemsToShow: 4 },
 ];
 
 const CarouselTeamLogo = () => {
   const [items, setItems] = useState([1, 2, 3, 4, 5, 6, 7, 8]);
 
   return (
-  <div>
+    <div>
       <hr className="seperator" />
       <div className="carousel-wrapper">
-        <Carousel breakPoints={breakPoints} className="h-[100px]">
+        <Carousel
+          breakPoints={breakPoints}
+          pagination={false}
+          className="h-[100px]"
+        >
           {items.map((item) => (
             <div className="w-[50px] h-[50px] border">{item}</div>
           ))}
