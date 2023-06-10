a77e3903320d226eacaafe90315e2504932c941b
wonhee418 <cheeky4@naver.com>
2023-06-11 01:15:20 +0900

    feat : ParticipatePlace 컴포넌트 캐러셀 적용

--- fika-front/src/components/hometown-pub-item/ParticipatePlace.tsx
+++ fika-front/src/components/hometown-pub-item/ParticipatePlace.tsx
@@ -1,8 +1,10 @@
+import React from "react";
 import Button from "src/ui/Button";
 import ParticipatePlaceItem from "./ParticipatePlaceItem";
 import { useState } from "react";
 import clsx from "clsx";
 import Chevrondown from "../../ui/icon/Chevrondown";
+import Carousel from "react-elastic-carousel";
 
 import placeImg from "../../assets/images/img.png";
 import profile01 from "../../assets/images/profile01.jpg";
@@ -10,8 +12,22 @@ import profile02 from "../../assets/images/profile02.jpg";
 import profile03 from "../../assets/images/profile03.jpg";
 import profile04 from "../../assets/images/profile04.jpg";
 import Link from "next/link";
+import { StaticImageData } from "next/image";
 
-const placeItem = [
+interface PlaceItem {
+  id: number;
+  placeImg: StaticImageData;
+  title: string;
+  schedule: string;
+  place: string;
+  location: string;
+  profileImg: StaticImageData[];
+  tag: string[];
+  Participants: string;
+  maxParticipants: string;
+}
+
+const places: PlaceItem[] = [
   {
     id: 1,
     placeImg: placeImg,
@@ -36,6 +52,30 @@ const placeItem = [
     Participants: "12",
     maxParticipants: "30",
   },
+  {
+    id: 3,
+    placeImg: placeImg,
+    title: "모임 제목",
+    schedule: "2월 27일 오후 10시(토)",
+    place: "가게이름",
+    location: "위치",
+    profileImg: [profile01, profile02, profile03, profile04],
+    tag: ["태그1", "태그2", "태그3"],
+    Participants: "12",
+    maxParticipants: "30",
+  },
+  {
+    id: 4,
+    placeImg: placeImg,
+    title: "모임 제목",
+    schedule: "2월 27일 오후 10시(토)",
+    place: "가게이름",
+    location: "위치",
+    profileImg: [profile01, profile02, profile03, profile04],
+    tag: ["태그1", "태그2", "태그3"],
+    Participants: "12",
+    maxParticipants: "30",
+  },
 ];
 
 const ParticipatePlace = () => {
@@ -44,6 +84,20 @@ const ParticipatePlace = () => {
   const toggleAccordion = () => {
     setIsExpanded(!isExpanded);
   };
+
+  const groupedPlaces: PlaceItem[][] = places.reduce(
+    (acc: PlaceItem[][], place: PlaceItem, index: number) => {
+      if (index % 2 === 0) {
+        acc.push([place]);
+      } else {
+        acc[acc.length - 1].push(place);
+      }
+
+      return acc;
+    },
+    []
+  );
+
   return (
     <div className="w-[490px] bg-white rounded-[10px] px-[25px] mb-[20px]">
       <div className="flex justify-between  py-[21.5px]">
@@ -60,14 +114,13 @@ const ParticipatePlace = () => {
           <Chevrondown></Chevrondown>
         </button>
       </div>
-      {/* TODO: 캐러셀 적용하기 */}
       <div
         className={clsx(
           "accordion-content ",
           isExpanded && "accordion-is-expanded"
         )}
       >
-        {!placeItem && (
+        {!places && (
           <div className="text-center py-[13px] px-[20px]">
             <p className="text-[15px font-medium leading-[1.5] pb-[19px]">
               참여 예정인 장소가 없습니다.
@@ -81,16 +134,17 @@ const ParticipatePlace = () => {
             </Button>
           </div>
         )}
-        {placeItem &&
-          placeItem.map((item) => (
-            <Link href={`hometown-pub/chatroom/${111}`}>
-              <ParticipatePlaceItem
-                {...item}
-                buttonValue="채팅방 입장"
-                key={item.id}
-              />
-            </Link>
+        <Carousel itemsToShow={1}>
+          {groupedPlaces.map((group, index) => (
+            <div className="slide w-full" key={`item-${index}`}>
+              {group.map((item) => (
+                <Link href={`hometown-pub/chatroom/${111}`} key={item.id}>
+                  <ParticipatePlaceItem {...item} buttonValue="채팅방 입장" />
+                </Link>
+              ))}
+            </div>
           ))}
+        </Carousel>
       </div>
     </div>
   );
